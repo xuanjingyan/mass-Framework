fa780e41408d75ebbde58f1a1bcb921198b031b6
xuanjingyan <930172575@qq.com>
2016-09-29 14:21:15 +0800

    css.js
    我就先看看，试试。增加了一个不需要的注释在开头的注释

--- css.js
+++ css.js
@@ -1,5 +1,5 @@
 //=========================================
-// 样式操作模块 v5 by 司徒正美
+// 样式操作模块 v5 by 司徒正美  我就看看
 //=========================================
 define("css", this.getComputedStyle ? ["node"] : ["css_fix"], function($) {
     var adapter = $.cssHooks || ($.cssHooks = {}),
